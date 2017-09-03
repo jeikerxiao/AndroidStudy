8a9cbf4339d8218e26524edc61acd6a951e9eb06
jeikerxiao <jeiker@126.com>
2017-09-03 16:03:34 +0800

    add Toast show
    add Toast show

--- Android-Sqlite/app/src/main/java/com/jeiker/android_sqlite/MainActivity.java
+++ Android-Sqlite/app/src/main/java/com/jeiker/android_sqlite/MainActivity.java
@@ -4,6 +4,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.jeiker.utils.Constant;
 import com.jeiker.utils.DbManger;
@@ -68,5 +69,6 @@ public class MainActivity extends AppCompatActivity {
                 db.close();
                 break;
         }
+        Toast.makeText(this, "数据库操作成功！", Toast.LENGTH_LONG).show();
     }
 }
