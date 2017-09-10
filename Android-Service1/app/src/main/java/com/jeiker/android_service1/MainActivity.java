77cbfc20409aff5ac96096003812b3341b98f81b
jeikerxiao <jeiker@126.com>
2017-09-10 10:18:45 +0800

    complete Service1 demo
    complete Service1 demo

--- Android-Service1/app/src/main/java/com/jeiker/android_service1/MainActivity.java
+++ Android-Service1/app/src/main/java/com/jeiker/android_service1/MainActivity.java
@@ -1,13 +1,50 @@
 package com.jeiker.android_service1;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private Button start;
+    private Button stop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        start = (Button) findViewById(R.id.btnStart);
+        stop = (Button) findViewById(R.id.btnStop);
+
+        //创建启动Service的Intent,以及Intent属性
+        final Intent intent = new Intent();
+        intent.setAction("com.jeiker.android_service1.TEST_SERVICE1");
+        intent.setPackage("com.jeiker.android_service1");
+
+        //为两个按钮设置点击事件,分别是启动与停止service
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService(intent);
+                Toast.makeText(MainActivity.this, "开始服务", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService(intent);
+                Toast.makeText(MainActivity.this, "结束服务", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
