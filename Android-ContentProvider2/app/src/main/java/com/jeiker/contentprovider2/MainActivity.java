57be39366f69d7fc9a221359c487be8a5c078cd4
jeikerxiao <jeiker@126.com>
2017-09-11 15:57:01 +0800

    complete ContentProvider2 demo
    complete ContentProvider2 demo

--- Android-ContentProvider2/app/src/main/java/com/jeiker/contentprovider2/MainActivity.java
+++ Android-ContentProvider2/app/src/main/java/com/jeiker/contentprovider2/MainActivity.java
@@ -2,16 +2,28 @@ package com.jeiker.contentprovider2;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.net.Uri;
+import android.os.ParcelFileDescriptor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.BufferedReader;
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final int READ_REQUEST_CODE = 42;
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +32,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button btnShow = (Button) findViewById(R.id.btn_show);
         btnShow.setOnClickListener(this);
+
+        mImageView = (ImageView) findViewById(R.id.image_view);
     }
 
     @Override
@@ -36,8 +50,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Uri uri;
             if (data != null) {
                 uri = data.getData();
+                // 显示图片
+                try {
+                    mImageView.setImageBitmap(getBitmapFromUri(uri));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 Log.e("MainActivity", "Uri: " + uri.toString());
             }
         }
     }
+
+    /**
+     * 根据Uri获得Bitmap
+     * @param uri
+     * @return
+     * @throws IOException
+     */
+    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor = getContentResolver().openFileDescriptor(uri, "r");
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+        return image;
+    }
+
+    /**
+     * 根据Uri获取输入流
+     * @param uri
+     * @return
+     * @throws IOException
+     */
+    private String readTextFromUri(Uri uri) throws IOException {
+        InputStream inputStream = getContentResolver().openInputStream(uri);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder stringBuilder = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            stringBuilder.append(line);
+        }
+//        fileInputStream.close();
+//        parcelFileDescriptor.close();
+        return stringBuilder.toString();
+    }
+
 }
