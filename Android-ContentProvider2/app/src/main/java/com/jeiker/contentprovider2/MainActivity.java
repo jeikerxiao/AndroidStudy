1be44714c13a7887ba62f1fe5a2a50683ee8ba0a
jeikerxiao <jeiker@126.com>
2017-09-11 14:18:25 +0800

    complete ContentProvider2 demo
    complete ContentProvider2 demo

--- Android-ContentProvider2/app/src/main/java/com/jeiker/contentprovider2/MainActivity.java
+++ Android-ContentProvider2/app/src/main/java/com/jeiker/contentprovider2/MainActivity.java
@@ -1,13 +1,43 @@
 package com.jeiker.contentprovider2;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int READ_REQUEST_CODE = 42;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnShow = (Button) findViewById(R.id.btn_show);
+        btnShow.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("image/*");
+        startActivityForResult(intent, READ_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            Uri uri;
+            if (data != null) {
+                uri = data.getData();
+                Log.e("MainActivity", "Uri: " + uri.toString());
+            }
+        }
     }
 }
