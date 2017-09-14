8d0c69f941d0c5527995e06c80c93d0c85dc3f77
jeikerxiao <jeiker@126.com>
2017-09-14 16:58:24 +0800

    complete ButterKinfe1 demo
    complete ButterKinfe1 demo

--- Android-ButterKinfe1/app/src/main/java/com/jeiker/butterkinfe1/MainActivity.java
+++ Android-ButterKinfe1/app/src/main/java/com/jeiker/butterkinfe1/MainActivity.java
@@ -2,12 +2,31 @@ package com.jeiker.butterkinfe1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_1) Button btn_1;
+    @BindView(R.id.btn_2) Button btn_2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    }
+    
+    @OnClick(R.id.btn_1) void btn1Click() {
+        Toast.makeText(this, "按钮1点击！", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.btn_2) void btn2Click() {
+        Toast.makeText(this, "按钮2点击！", Toast.LENGTH_SHORT).show();
     }
 }
