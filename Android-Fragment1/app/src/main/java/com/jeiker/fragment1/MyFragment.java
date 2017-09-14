d20852f9aa2a56e7809d77207e35ba692a5b97a0
jeikerxiao <jeiker@126.com>
2017-09-14 14:21:07 +0800

    not complete Fragment1
    not complete Fragment1

---
+++ Android-Fragment1/app/src/main/java/com/jeiker/fragment1/MyFragment.java
@@ -0,0 +1,29 @@
+package com.jeiker.fragment1;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by xiao on 17/9/14.
+ */
+
+public class MyFragment extends Fragment {
+
+    private String content;
+
+    public MyFragment(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(android.R.layout.fg_content, container, false);
+        TextView txt_content = (TextView) view.findViewById(R.id.txt_content);
+        txt_content.setText(content);
+        return view;
+    }
+}
